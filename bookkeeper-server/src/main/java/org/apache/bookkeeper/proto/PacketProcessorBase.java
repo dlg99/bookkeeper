7255e0cae1a33e0dda461b2a20d40e8bfd529f87
Yong Zhang <zhangyong1025.zy@gmail.com>
2022-06-16 08:17:19 +0800

    Fix compile

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/PacketProcessorBase.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/PacketProcessorBase.java
@@ -19,6 +19,7 @@ package org.apache.bookkeeper.proto;
 
 import io.netty.channel.Channel;
 
+import io.netty.channel.ChannelPromise;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.bookkeeper.proto.BookieProtocol.Request;
